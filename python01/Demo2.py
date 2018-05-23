82f9c78f735d161e2b7a84060a2827c362ae638b
孙小少 <1029574135@qq.com>
2018-05-23 23:30:16 +0800

    while

---
+++ python01/Demo2.py
@@ -0,0 +1,8 @@
+x = 1949
+while x <= 2018:
+	if x % 4 == 0:
+		if x % 400 == 0:
+			print(x)
+		else:
+			print(x)
+	x += 1
---
+++ python01/Demo5.py
@@ -0,0 +1,7 @@
+x = '*'
+y = ' '
+for i in range(6):
+	print(y * (6 - i),x * (2 * i + 1))
+	
+for i in range(5):
+	print(y * (i + 2),x * (9-2*i))
